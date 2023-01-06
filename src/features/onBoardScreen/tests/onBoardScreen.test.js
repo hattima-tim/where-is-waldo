0913ddb167c51ac66969de2cfdb2296d40f1fff2
hattima-tim <irfanridwan20@gmail.com>
2023-01-06 15:25:03 +0000

    format

--- src/features/onBoardScreen/tests/onBoardScreen.test.js
+++ src/features/onBoardScreen/tests/onBoardScreen.test.js
@@ -1,16 +1,16 @@
 import { render } from "@testing-library/react";
 import OnBoardScreen from "../onBoardScreen";
 
-jest.mock('../character',()=>({characterName})=>{
-    return <p>{characterName}</p>
+jest.mock("../character", () => ({ characterName }) => {
+  return <p>{characterName}</p>;
 });
 
-jest.mock('../userInstructionCard',()=>()=>{
-    return <p>User Instruction Card</p>
-})
+jest.mock("../userInstructionCard", () => () => {
+  return <p>User Instruction Card</p>;
+});
 
-test('renders onBoardScreen correctly',()=>{
-    const {container} = render(<OnBoardScreen/>);
+test("renders onBoardScreen correctly", () => {
+  const { container } = render(<OnBoardScreen />);
 
-    expect(container).toMatchSnapshot();
-})
+  expect(container).toMatchSnapshot();
+});
