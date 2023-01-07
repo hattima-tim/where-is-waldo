26b1cc94fd7d5a076f5589774ea2dae2520175eb
hattima-tim <irfanridwan20@gmail.com>
2023-01-07 13:16:26 +0000

    make onBoardScreen position fixed
    to position it over the game screen

--- src/features/onBoardScreen/onBoardScreen.js
+++ src/features/onBoardScreen/onBoardScreen.js
@@ -5,7 +5,7 @@ export default function OnBoardScreen() {
   const [isGameOn, setIsGameOn] = useState(false);
 
   return isGameOn ? null : (
-    <div className="z-10 flex w-full flex-col items-center justify-center gap-6 bg-[rgba(0,0,0,0.9)]">
+    <div className="fixed h-full lg:p-20 z-10 flex w-full flex-col items-center justify-center gap-6 bg-[rgba(0,0,0,0.9)]">
       <h1 className="mt-8 text-lg font-bold text-white">Where's Waldo</h1>
       <p className="text-white">Find these characters on universe 113!</p>
       <UserInstructionCard setIsGameOn={setIsGameOn} />
