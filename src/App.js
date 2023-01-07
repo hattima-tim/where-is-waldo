2cd64a54133d7c15d3f974dcfd6dd28f16bdff1e
hattima-tim <irfanridwan20@gmail.com>
2023-01-07 16:56:33 +0000

    add GameScreen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import OnBoardScreen from "./features/onBoardScreen/onBoardScreen";
+import GameScreen from "./features/gameScreen/gameScreen";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <OnBoardScreen/>
+      <GameScreen/>
     </div>
   );
 }
