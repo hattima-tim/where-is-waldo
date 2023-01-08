bd97ce4926906a4b712eef62e164d5858afc65f3
hattima-tim <irfanridwan20@gmail.com>
2023-01-08 15:59:46 +0000

    show character tooltip on click on 3 of header

--- src/features/gameScreen/gameScreen.js
+++ src/features/gameScreen/gameScreen.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import Character from "../onBoardScreen/character";
 
 export default function GameScreen() {
   const [counter, setCounter] = useState("00.00.00");
+  const [showCharacterListTooltip, setShowCharacterListTooltip] =
+    useState(false);
 
   return (
     <div>
@@ -12,8 +15,49 @@ export default function GameScreen() {
         <div role={"timer"} className="text-2xl font-bold text-white">
           {counter}
         </div>
-        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#8d0c0c] text-2xl text-white">
-          3
+        <div>
+          <div
+            className="flex h-12 w-12 items-center justify-center rounded-full bg-[#8d0c0c] text-2xl text-white"
+            onClick={() =>
+              setShowCharacterListTooltip(!showCharacterListTooltip)
+            }
+          >
+            3
+          </div>
+
+          {showCharacterListTooltip && (
+            <div className="absolute right-40 z-10 w-72 rounded-md bg-[#17134d] p-4 text-white transition-all">
+              <Character
+                difficultyLevel={"easy"}
+                src={
+                  "https://res.cloudinary.com/du3oueesv/image/upload/v1672759163/Where%27s%20Waldo/cacodemon.1dad269c_ajnuga.png"
+                }
+                alt={"cacodemon"}
+                characterName={"Cacodemon"}
+                reference={"Doom"}
+              />
+
+              <Character
+                difficultyLevel={"medium"}
+                src={
+                  "https://res.cloudinary.com/du3oueesv/image/upload/v1672759181/Where%27s%20Waldo/bill-cipher.1e7e0fc4_jfln6r.png"
+                }
+                alt={"bill cipher"}
+                characterName={"Bill Cipher"}
+                reference={"Gravity Falls"}
+              />
+
+              <Character
+                difficultyLevel={"hard"}
+                src={
+                  "https://res.cloudinary.com/du3oueesv/image/upload/v1672759200/Where%27s%20Waldo/courage.200fe7a8_jkf3vs.png"
+                }
+                alt={"courage"}
+                characterName={"Courage"}
+                reference={"Courage the Cowardly Dog"}
+              />
+            </div>
+          )}
         </div>
       </header>
       <img
