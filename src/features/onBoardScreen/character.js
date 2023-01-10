1975c4d43105538d8de149414114cd6cab928c9d
hattima-tim <irfanridwan20@gmail.com>
2023-01-10 05:15:10 +0000

    create Characters component

--- src/features/onBoardScreen/character.js
+++ src/features/onBoardScreen/character.js
@@ -1,20 +1,48 @@
-export default function Character({
-  difficultyLevel,
-  src,
-  alt,
-  characterName,
-  reference,
-}) {
+export default function Characters() {
+  const characters = [
+    {
+      difficultyLevel: "easy",
+      src: "https://res.cloudinary.com/du3oueesv/image/upload/v1672759163/Where%27s%20Waldo/cacodemon.1dad269c_ajnuga.png",
+      alt: "cacodemon",
+      name: "Cacodemon",
+      reference: "Doom",
+    },
+    {
+      difficultyLevel: "medium",
+      src: "https://res.cloudinary.com/du3oueesv/image/upload/v1672759181/Where%27s%20Waldo/bill-cipher.1e7e0fc4_jfln6r.png",
+      alt: "bill cipher",
+      name: "Bill Cipher",
+      reference: "Gravity Falls",
+    },
+    {
+      difficultyLevel: "hard",
+      src: "https://res.cloudinary.com/du3oueesv/image/upload/v1672759200/Where%27s%20Waldo/courage.200fe7a8_jkf3vs.png",
+      alt: "courage",
+      name: "Courage",
+      reference: "Courage the Cowardly Dog",
+    },
+  ];
+
   return (
-    <div className="flex w-full flex-col">
-      <p className="self-end">{difficultyLevel}</p>
-      <div className="flex gap-4">
-        <img src={src} alt={alt} className="w-8"></img>
-        <div>
-          <h4>{characterName}</h4>
-          <p>{reference}</p>
-        </div>
-      </div>
+    <div>
+      {characters.map((character) => {
+        return (
+          <div className="flex w-full flex-col">
+            <p className="self-end">{character.difficultyLevel}</p>
+            <div className="flex gap-4">
+              <img
+                src={character.src}
+                alt={character.alt}
+                className="w-8"
+              ></img>
+              <div>
+                <h4>{character.name}</h4>
+                <p>{character.reference}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
