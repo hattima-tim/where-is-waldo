1b8f2232e47771d2a988f47880a49be931e46de6
hattima-tim <irfanridwan20@gmail.com>
2023-01-10 13:07:03 +0000

    update snapshot for Characters component

--- src/features/onBoardScreen/tests/character.test.js
+++ src/features/onBoardScreen/tests/character.test.js
@@ -1,17 +1,9 @@
 import { render } from "@testing-library/react";
-import Character from "../character";
+import Characters from "../characters";
 
 test("Character component is rendered correctly", () => {
   const { container } = render(
-    <Character
-      difficultyLevel={"easy"}
-      src={
-        "https://res.cloudinary.com/du3oueesv/image/upload/v1672759163/Where%27s%20Waldo/cacodemon.1dad269c_ajnuga.png"
-      }
-      alt={"cacodemon"}
-      characterName={"Cacodemon"}
-      reference={"Doom"}
-    />
+    <Characters />
   );
 
   expect(container).toMatchSnapshot();
