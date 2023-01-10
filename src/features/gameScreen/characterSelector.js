ff9f203012da134fc3dc8ff605d66550ea83b7d8
hattima-tim <irfanridwan20@gmail.com>
2023-01-10 11:13:23 +0000

    add characters option for selecting a character

--- src/features/gameScreen/characterSelector.js
+++ src/features/gameScreen/characterSelector.js
@@ -1,12 +1,17 @@
-export default function CharacterSelector({location}) {
+import Characters from "../onBoardScreen/characters";
+
+export default function CharacterSelector({ location }) {
   return (
-    <div>
+    <div className="flex" style={location}>
       <img
         src="https://res.cloudinary.com/du3oueesv/image/upload/v1673284323/Where%27s%20Waldo/Where_s_Waldo_dot_k9q85x-media_lib_thumb_i9xieg.png"
         alt="targetCircle"
-        style={location}
+        className="max-h-16 w-16"
       ></img>
-      
+
+      <div className="z-10 w-72 rounded-md bg-[#17134d] p-4 text-white transition-all">
+        <Characters />
+      </div>
     </div>
   );
 }
