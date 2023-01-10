7288d0fc2d80cb760efd3de99ff3d59bf30c805a
hattima-tim <irfanridwan20@gmail.com>
2023-01-10 05:15:32 +0000

    add Characters component

--- src/features/onBoardScreen/userInstructionCard.js
+++ src/features/onBoardScreen/userInstructionCard.js
@@ -1,6 +1,6 @@
-import Character from "./character";
+import Characters from "./character";
 
-export default function UserInstructionCard({setIsGameOn}) {
+export default function UserInstructionCard({ setIsGameOn }) {
   return (
     <div className="flex w-11/12 border bg-white lg:w-3/5">
       <img
@@ -11,40 +11,11 @@ export default function UserInstructionCard({setIsGameOn}) {
       <div className="flex flex-col items-center gap-4 p-4">
         <h2>Universe 113</h2>
         <p>by Egor Klyuchnyk</p>
-        <Character
-          difficultyLevel={"easy"}
-          src={
-            "https://res.cloudinary.com/du3oueesv/image/upload/v1672759163/Where%27s%20Waldo/cacodemon.1dad269c_ajnuga.png"
-          }
-          alt={"cacodemon"}
-          characterName={"Cacodemon"}
-          reference={"Doom"}
-        />
-
-        <Character
-          difficultyLevel={"medium"}
-          src={
-            "https://res.cloudinary.com/du3oueesv/image/upload/v1672759181/Where%27s%20Waldo/bill-cipher.1e7e0fc4_jfln6r.png"
-          }
-          alt={"bill cipher"}
-          characterName={"Bill Cipher"}
-          reference={"Gravity Falls"}
-        />
-
-        <Character
-          difficultyLevel={"hard"}
-          src={
-            "https://res.cloudinary.com/du3oueesv/image/upload/v1672759200/Where%27s%20Waldo/courage.200fe7a8_jkf3vs.png"
-          }
-          alt={"courage"}
-          characterName={"Courage"}
-          reference={"Courage the Cowardly Dog"}
-        />
-
-        <button 
+        <Characters />
+        <button
           className="rounded-full bg-gradient-to-r from-[#2a2c80] via-[#fd1d1d] to-[#fcb045] py-2 px-4 font-bold uppercase text-white transition-transform duration-300 ease-in-out hover:scale-110"
-          onClick={()=>setIsGameOn(true)}
-          >
+          onClick={() => setIsGameOn(true)}
+        >
           Start
         </button>
       </div>
