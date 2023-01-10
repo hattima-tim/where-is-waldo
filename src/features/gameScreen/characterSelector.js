d6ffd6fcbf37b5770d18bb8ef1895d5182bfed04
hattima-tim <irfanridwan20@gmail.com>
2023-01-10 04:52:07 +0000

    show a circle on target area

--- src/features/gameScreen/characterSelector.js
+++ src/features/gameScreen/characterSelector.js
@@ -1,7 +1,12 @@
-export default function CharacterSelector() {
+export default function CharacterSelector({location}) {
   return (
     <div>
-
+      <img
+        src="https://res.cloudinary.com/du3oueesv/image/upload/v1673284323/Where%27s%20Waldo/Where_s_Waldo_dot_k9q85x-media_lib_thumb_i9xieg.png"
+        alt="targetCircle"
+        style={location}
+      ></img>
+      
     </div>
   );
 }
