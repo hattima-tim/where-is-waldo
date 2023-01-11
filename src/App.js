8d56a47b0854ad18992dee228c3b0d9b4e26b1b8
hattima-tim <irfanridwan20@gmail.com>
2023-01-11 12:37:31 +0000

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { initializeApp } from "firebase/app";
 import OnBoardScreen from "./features/onBoardScreen/onBoardScreen";
 import GameScreen from "./features/gameScreen/gameScreen";
 import "./App.css";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAjoovvOyvoRK2x9VTh6QW2et9ESVSPkfE",
+  authDomain: "where-s-waldo-1017a.firebaseapp.com",
+  projectId: "where-s-waldo-1017a",
+  storageBucket: "where-s-waldo-1017a.appspot.com",
+  messagingSenderId: "214691130134",
+  appId: "1:214691130134:web:fd5f90f243727caf8abe37",
+  measurementId: "G-T8E27ZKPDP",
+};
+
+initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
-      <OnBoardScreen/>
-      <GameScreen/>
+      <OnBoardScreen />
+      <GameScreen />
     </div>
   );
 }
