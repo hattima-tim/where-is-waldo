cade86e893ba8d56b9091a1d2b1dffe7bf635647
hattima-tim <irfanridwan20@gmail.com>
2023-01-26 16:28:01 +0000

    update tests

--- src/features/gameScreen/tests/gameScreen.test.js
+++ src/features/gameScreen/tests/gameScreen.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { async } from "q";
 import { toMatchDiffSnapshot } from "snapshot-diff";
 import GameScreen from "../gameScreen";
 
