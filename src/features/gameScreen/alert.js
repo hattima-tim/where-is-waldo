ad59517f7d8aa267256f913dabeac07198e1ea47
hattima-tim <irfanridwan20@gmail.com>
2023-01-30 11:26:46 +0000

    show 'see scoreboard' button in alert

--- src/features/gameScreen/alert.js
+++ src/features/gameScreen/alert.js
@@ -130,14 +130,31 @@ export default function Alert({
               </strong>
 
               <p className="mt-1 text-sm text-gray-700">{selectionResult}</p>
+              
+              <button className="inline-flex mt-2 items-center gap-2 rounded-lg bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600">
+                <span className="text-sm"> See Scoreboard </span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-4 w-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
+                </svg>
+              </button>
             </div>
 
             <button
               className="text-gray-500 transition hover:text-gray-600"
               onClick={handleAlertRemoval}
             >
-              <span className="sr-only">Dismiss popup</span>
-
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
