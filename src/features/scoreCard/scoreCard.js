62f679df2d96fdd0b8e24a33b5718a02d415a35a
hattima-tim <irfanridwan20@gmail.com>
2023-01-30 11:03:42 +0000

    show top 10 users on scoreboard

--- src/features/scoreCard/scoreCard.js
+++ src/features/scoreCard/scoreCard.js
@@ -1,5 +1,11 @@
-import { useEffect } from "react";
-import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+  getDocs,
+  collection,
+} from "firebase/firestore";
 
 export default function ScoreCard({
   userId,
@@ -10,9 +16,9 @@ export default function ScoreCard({
 }) {
   useEffect(() => {
     const timeTookForFinishingTheGame = {
-      milliSecond: msCounter,
-      second: secondCounter,
-      minute: minuteCounter,
+      milliSecond: Number(msCounter),
+      second: Number(secondCounter),
+      minute: Number(minuteCounter),
     };
 
     async function pushUserInfoToFirebase() {
@@ -33,22 +39,59 @@ export default function ScoreCard({
     pushUserInfoToFirebase();
   }, [userName, userId, msCounter, secondCounter, minuteCounter]);
 
+  const [allUsersInfo, setAllUsersInfo] = useState([]);
+  const getTopTenUsers = () => {
+    const allUsersInfoCopy = [...allUsersInfo];
+
+    const sortedUsers = allUsersInfoCopy.sort((a, b) => {
+      const scoreOfA = a.timeTookForFinishingTheGame;
+      const scoreOfB = b.timeTookForFinishingTheGame;
+      return scoreOfA.minute > scoreOfB.minute ||
+        scoreOfA.second > scoreOfB.second ||
+        scoreOfA.milliSecond > scoreOfB.milliSecond
+        ? 1 // sort a after b
+        : -1; // sort a before b
+    });
+    sortedUsers.splice(10);
+
+    return sortedUsers;
+  };
+
+  useEffect(() => {
+    async function getUsersInfoFromFirestore() {
+      const db = getFirestore();
+      const usersCollectionRef = collection(db, "users");
+
+      try {
+        const allUsersInfoCopy = [];
+        const usersSnapshot = await getDocs(usersCollectionRef);
+        usersSnapshot.forEach((doc) => {
+          allUsersInfoCopy.push(doc.data());
+        });
+
+        setAllUsersInfo(allUsersInfoCopy);
+      } catch (error) {
+        alert("Something went wrong!");
+      }
+    }
+    getUsersInfoFromFirestore();
+  }, []);
+
+  const topTenUsers = getTopTenUsers(allUsersInfo);
+
   return (
     <div className="absolute z-50  flex h-full w-full items-center justify-center">
       <div className="flex items-center justify-around rounded-2xl bg-white p-12 sm:w-11/12 lg:w-1/2">
         <div>
           <h1 className="bold text-3xl">High Scores</h1>
           <ol className="m-4 list-decimal">
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
-            <li>EWW 00:00:00</li>
+            {topTenUsers.map((user) => (
+              <li key={user.id}>
+                {user.name} {user.timeTookForFinishingTheGame.minute}:
+                {user.timeTookForFinishingTheGame.second}:
+                {user.timeTookForFinishingTheGame.milliSecond}
+              </li>
+            ))}
           </ol>
         </div>
         <div className="flex flex-col items-center gap-4">
