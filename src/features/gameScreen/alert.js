df2579e9483d5e75e9be6950dfc87b976ecb823c
hattima-tim <irfanridwan20@gmail.com>
2023-01-31 11:21:17 +0000

    don't show remove alert button

--- src/features/gameScreen/alert.js
+++ src/features/gameScreen/alert.js
@@ -154,26 +154,6 @@ export default function Alert({
                 </svg>
               </button>
             </div>
-
-            <button
-              className="text-gray-500 transition hover:text-gray-600"
-              onClick={handleAlertRemoval}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       )}
