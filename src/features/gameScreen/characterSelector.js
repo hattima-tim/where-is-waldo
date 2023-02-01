108fcd42c8bba96444b6593f9890fe733631ecfe
hattima-tim <irfanridwan20@gmail.com>
2023-02-01 04:25:05 +0000

    style CharacterSelector component

--- src/features/gameScreen/characterSelector.js
+++ src/features/gameScreen/characterSelector.js
@@ -1,14 +1,20 @@
 import Characters from "../onBoardScreen/characters";
 
-export default function CharacterSelector({ location, handleCharacterSelection}) {
+export default function CharacterSelector({
+  location,
+  handleCharacterSelection,
+}) {
   return (
-    <div className="flex" style={location}>
+    <div className="flex flex-col gap-1 lg:flex-row" style={location}>
       <div className="flex h-16 w-16 items-center  justify-center rounded-full border-4 border-dashed border-white bg-[hsla(0,0%,100%,.3)]">
         <div className="relative h-1 w-1 rounded-full bg-red-600"></div>
       </div>
 
-      <div className="z-10 w-72 rounded-md bg-[#17134d] p-4 text-white transition-all">
-        <Characters handleCharacterSelection={handleCharacterSelection} />
+      <div className="z-10 rounded-md bg-[#17134d] p-4 text-white transition-all lg:p-5">
+        <Characters
+          handleCharacterSelection={handleCharacterSelection}
+          componentNeededFor={"selector"}
+        />
       </div>
     </div>
   );
