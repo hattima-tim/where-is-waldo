c5052f86e07dc2d42152cb6a5c94ab5887add4d0
hattima-tim <irfanridwan20@gmail.com>
2023-02-02 07:02:54 +0000

    update z-index of onboard screen
    to make it appear on top of header

--- src/features/onBoardScreen/onBoardScreen.js
+++ src/features/onBoardScreen/onBoardScreen.js
@@ -2,7 +2,7 @@ import UserInstructionCard from "./userInstructionCard";
 
 export default function OnBoardScreen({ startTheGame }) {
   return (
-    <div className="fixed z-10 flex h-full w-full flex-col items-center justify-center gap-6 bg-[rgba(0,0,0,0.9)] lg:p-20">
+    <div className="fixed z-20 flex h-full w-full flex-col items-center justify-center gap-6 bg-[rgba(0,0,0,0.9)] lg:p-20">
       <h1 className="mt-8 text-lg font-bold text-white">Where's Waldo</h1>
       <p className="text-white">Find these characters on universe 113!</p>
       <UserInstructionCard startTheGame={startTheGame} />
