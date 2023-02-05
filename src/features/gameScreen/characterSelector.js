c9993818992ca7d18beeba918c91cd6f22b9718b
hattima-tim <irfanridwan20@gmail.com>
2023-02-05 15:01:20 +0000

    change the color of character selector circle

--- src/features/gameScreen/characterSelector.js
+++ src/features/gameScreen/characterSelector.js
@@ -6,7 +6,7 @@ export default function CharacterSelector({
 }) {
   return (
     <div className="flex flex-col gap-1 lg:flex-row" style={location}>
-      <div className="flex h-16 w-16 items-center  justify-center rounded-full border-4 border-dashed border-white bg-[hsla(0,0%,100%,.3)]">
+      <div className="flex h-16 w-16 items-center  justify-center rounded-full border-4 border-dashed border-red-600">
         <div className="relative h-1 w-1 rounded-full bg-red-600"></div>
       </div>
 
